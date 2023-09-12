8e28bcdbcc2066107119076006b3b0f88526cce7
msbah544 <momodoubah544@gmail.com>
2023-09-12 13:02:42 +0000

    implemented token generation after successful login/signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,19 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 //signup user
+
+const generateToken = (_id) => {
+  return jwt.sign({ id: _id }, process.env.JWT_SECRET);
+};
 
 export const userSignup = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
     if (user) {
-      return res.status(200).json(user);
+      const token = generateToken(user._id);
+      return res.status(200).json({ email: user.email, token: token });
     }
   } catch (error) {
     return res.status(400).json({ message: `faliure: ${error.message}` });
@@ -23,7 +29,8 @@ export const userLogin = async (req, res) => {
     const user = await User.login(email, password);
 
     if (user) {
-      return res.status(200).json(user);
+      const token = generateToken(user._id);
+      return res.status(200).json({ email: user.email, token: token });
     }
   } catch (error) {
     return res.status(400).json({ message: ` faliure: ${error.message}` });
